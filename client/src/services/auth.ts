6672084d5cbba8c17aaae3ee21d5f60716b7d881
Ryan <ryan.irani@gmail.com>
2024-10-19 09:00:12 +0100

    added verification of token

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,4 +1,23 @@
+import { jwtDecode } from 'jwt-decode'
+
 export function isAuthenticated() {
   const token = localStorage.getItem('token')
-  return !!token
+
+  if (!token) {
+    return false
+  }
+
+  try {
+    const decoded = jwtDecode(token)
+    const currentTime = Date.now() / 1000
+
+    if (decoded.exp < currentTime) {
+      console.log('Token has expired')
+      return false
+    }
+    return true
+  } catch (err) {
+    console.log('Invalid token', err)
+    return false
+  }
 }
