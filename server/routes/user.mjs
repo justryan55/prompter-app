1a6c5cc167ab6d79c093ec680f04e9824de241d7
Ryan <ryan.irani@gmail.com>
2024-10-19 16:24:01 +0100

    added user route

---
+++ server/routes/user.mjs
@@ -0,0 +1,33 @@
+import express from 'express'
+import userModel from '../models/userModel.mjs'
+
+const router = express.Router()
+
+router.get('/getCurrentUser/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId
+    const user = await userModel.findOne({ _id: userId })
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'Unable to find user.'
+      })
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: {
+        userId: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        myCircle: user.myCircle
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+export default router
--- server/server.mjs
+++ server/server.mjs
@@ -1,6 +1,7 @@
 import express from 'express'
 import auth from '../server/routes/auth.mjs'
 import connections from '../server/routes/connections.mjs'
+import user from '../server/routes/user.mjs'
 
 import cors from 'cors'
 import connectDatabase from './config/db.mjs'
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/api/auth', auth)
 app.use('/api/', connections)
+app.use('/api', user)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
