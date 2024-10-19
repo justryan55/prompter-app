66b2e4bef186b42f4f9f4d4840ea60ab0b53bf70
Ryan <ryan.irani@gmail.com>
2024-10-19 09:01:11 +0100

    changed myCircle type to array of objects

--- server/models/userModel.mjs
+++ server/models/userModel.mjs
@@ -6,7 +6,7 @@ const userSchema = new Schema(
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    myCircle: [{ type: String }]
+    myCircle: [{ type: Object }]
   },
   { timestamps: true }
 )
