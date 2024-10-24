6fe1a1a9238f3d735be65afafc6be5eaa184ec1d
Ryan <ryan.irani@gmail.com>
2024-10-24 08:44:56 +0100

    added messages schema

--- server/models/userModel.mjs
+++ server/models/userModel.mjs
@@ -1,12 +1,22 @@
 import { Schema, mongoose } from 'mongoose'
 
+const messageSchema = new Schema(
+  {
+    sender: [{ type: String }],
+    prompt: { type: String },
+    message: { type: Object }
+  },
+  { timestamps: true }
+)
+
 const userSchema = new Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    myCircle: [{ type: Object }]
+    myCircle: [{ type: Object }],
+    messages: { type: [messageSchema], default: [] }
   },
   { timestamps: true }
 )
