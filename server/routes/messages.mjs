4de2d6e0b28b3729b6f963aa46aa88ee45fc5025
Ryan <ryan.irani@gmail.com>
2024-10-28 17:24:22 +0000

    added add-response and fetch-messages routes

--- server/routes/messages.mjs
+++ server/routes/messages.mjs
@@ -14,18 +14,71 @@ router.post('/:userId/:connectionId/new-message', async (req, res) => {
     const message = {
       sender: [payload.sender.userId, payload.sender.firstName, payload.sender.lastName],
       prompt: payload.prompt,
-      message: payload.message
+      message: payload.message,
+      responses: []
     }
 
     user.messages.push(message)
     connection.messages.push(message)
-
     await user.save()
     await connection.save()
 
+    const messageId = user.messages[user.messages.length - 1].id
+
     return res.status(200).json({
       success: true,
-      message: 'Prompt response sent'
+      message: payload.message,
+      messageId: messageId
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.get('/:userId/messages/:messageId', async (req, res) => {
+  try {
+    const { messageId, userId } = req.params
+
+    const user = await userModel.findById(userId)
+
+    const messages = user.messages
+
+    const message = messages.map((message) => {
+      if (messageId === message.id) {
+        return res.status(200).json({
+          success: true,
+          message: message
+        })
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.post('/:userId/messages/:messageId/add-response', async (req, res) => {
+  try {
+    const { messageId, userId } = req.params
+    const payload = req.body
+    const user = await userModel.findById(userId)
+
+    const message = user.messages.find((msg) => msg.id === messageId)
+
+    if (!message) {
+      return res.status(404).json({
+        success: false,
+        message: 'Message not found'
+      })
+    }
+
+    console.log(message.responses)
+
+    message.responses.push(payload)
+    await user.save()
+
+    return res.status(200).json({
+      success: true,
+      message: message
     })
   } catch (err) {
     console.log(err)
