ba22f0ee17005e2441cb36c3d0f04a9ac24b8c42
Ryan <ryan.irani@gmail.com>
2024-10-28 07:57:22 +0000

    changed route and new message now sent to both parties

--- server/routes/messages.mjs
+++ server/routes/messages.mjs
@@ -3,12 +3,13 @@ import userModel from '../models/userModel.mjs'
 
 const router = express.Router()
 
-router.post('/:userId/:connectionId/new-prompt', async (req, res) => {
+router.post('/:userId/:connectionId/new-message', async (req, res) => {
   try {
     const payload = req.body
     const { userId, connectionId } = req.params
 
     const user = await userModel.findById(userId)
+    const connection = await userModel.findById(connectionId)
 
     const message = {
       sender: [payload.sender.userId, payload.sender.firstName, payload.sender.lastName],
@@ -17,8 +18,10 @@ router.post('/:userId/:connectionId/new-prompt', async (req, res) => {
     }
 
     user.messages.push(message)
+    connection.messages.push(message)
 
     await user.save()
+    await connection.save()
 
     return res.status(200).json({
       success: true,
