f58228d6e561bd87de20d8a4eae292e7de5d290d
Ryan <ryan.irani@gmail.com>
2024-10-31 14:37:59 +0000

    commented out the explore icon

--- client/src/components/NavigationBar.vue
+++ client/src/components/NavigationBar.vue
@@ -40,7 +40,7 @@ const isCurrentRoute = (path) => {
         />
       </svg>
     </div>
-    <div>
+    <!-- <div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -60,7 +60,7 @@ const isCurrentRoute = (path) => {
           d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
         />
       </svg>
-    </div>
+    </div> -->
     <div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
